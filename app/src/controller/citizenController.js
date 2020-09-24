e5f567ebe95554d45fa6bae0532a8a57e8f24590
Joshua Parker <joshua.parker@engineering.digital.dwp.gov.uk>
2020-09-24 23:18:44 +0100

    chore: renamed app.js to server.js

--- app/src/controller/citizenController.js
+++ app/src/controller/citizenController.js
@@ -7,21 +7,21 @@ module.exports = (citizenService) => {
     const successMessage = 'Successfully retrieved all citizens'
 
     return await citizenService.getAllCitizens()
-      .then(citizens => sendSuccessfulResponse(200, successMessage, citizens))
-      .catch(err => sendUnSuccessfulResponse(err, req))
+      .then(citizens => constructSuccessfulHttpResponse(200, successMessage, citizens))
+      .catch(err => constructUnSuccessfulHttpResponse(500, err, req))
   }
 
-  const sendSuccessfulResponse = (statusCode, message, data) => {
+  const constructSuccessfulHttpResponse = (statusCode, message, data) => {
     const successBody = new SuccessBody(true, message, data)
     const headers = [{'Content-Type': 'application/json'}]
     return new HttpResponse(statusCode, headers, successBody)
   }
 
-  const sendUnSuccessfulResponse = (err, req) => {
+  const constructUnSuccessfulHttpResponse = (status, err, req) => {
     if (err instanceof InternalServerException) {
       const errorBody = new ErrorBody(false, err.message, 'detailed message', req.path)
       const headers = [{'Content-Type': 'application/json'}]
-      return new HttpResponse(500, headers, errorBody)
+      return new HttpResponse(status, headers, errorBody)
     }
   }
 
